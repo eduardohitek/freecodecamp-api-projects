c405475ef391cdd802062988732922470275a352
Eduardo Hitek <eduardohitek@gmail.com>
2017-06-06 23:30:43 +0000

    Changing the route

--- freecodecamp-whoiam/server.js
+++ freecodecamp-whoiam/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 
-app.use("/", function(req, res){
+app.use("/api/whoami", function(req, res){
     const ipAddress = req.headers['x-forwarded-for']
     const software = req.headers['user-agent'].split('(')[1].split(')')[0]
     const language = req.headers['accept-language'].split(',')[0]
